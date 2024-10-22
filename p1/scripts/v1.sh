ec45b8d210193c2daa918bdea6433d34aa09b360
het-tale <ettalebyhasnaa99@gmail.com>
2024-10-22 11:30:12 +0100

    feat: install k3s in both machines

--- p1/scripts/v1.sh
+++ p1/scripts/v1.sh
@@ -1,3 +1,27 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "Hello from v1.sh"
+# Install K3s in controller mode
+curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110 --flannel-iface=eth1
+
+# Wait for K3s to be ready
+while ! sudo k3s kubectl get node &>/dev/null; do
+    sleep 1
+done
+
+# Get the node token
+NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+
+# Make the node token available to the worker node
+echo $NODE_TOKEN > /vagrant/node-token
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Set up kubectl for the vagrant user
+mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
+echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+
+echo "K3s controller and kubectl have been installed and configured."
--- p1/scripts/v2.sh
+++ p1/scripts/v2.sh
@@ -1,3 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "Hello from v2.sh"
+# Wait for the node token to be available
+while [ ! -f /vagrant/node-token ]; do
+    sleep 1
+done
+
+# Get the node token
+NODE_TOKEN=$(cat /vagrant/node-token)
+
+# Install K3s in agent mode
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$NODE_TOKEN sh -s - --node-ip=192.168.56.111 --flannel-iface=eth1
+
+echo "K3s agent has been installed and configured."
